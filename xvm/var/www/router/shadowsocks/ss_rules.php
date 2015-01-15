a6bf6a85fa3982f8ebde1280d2e527eaf3ee5e26
70599 <w70599@gmail.com>
2015-01-15 16:00:23 -0500

    xvm daily bkup

--- xvm/var/www/router/shadowsocks/ss_rules.php
+++ xvm/var/www/router/shadowsocks/ss_rules.php
@@ -1,7 +1,7 @@
 <?php
 	include '../../common.php';
 
-	$updatetime_page = filemtime("n750.php");
+	$updatetime_page = filemtime("ss_rules.php");
 ?>
 
 <!DOCTYPE html>
@@ -10,13 +10,141 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<link rel="stylesheet" type="text/css" href="../../style.css">
 		<title>使用ss-rules透明翻墙的步骤</title>
+		<script src="../../js/jquery-1.11.1.min.js"></script>
+		<script type="text/javascript">
+			jQuery(document).ready(function() {
+				jQuery('.tabs .tab-links a').on('click', function(e)  {
+					var currentAttrValue = jQuery(this).attr('href');
+
+					// Show/Hide Tabs
+					jQuery('.tabs ' + currentAttrValue).show().siblings().hide();
+
+					// Change/remove current tab to active
+					jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+
+					e.preventDefault();
+				});
+			});
+		</script>
 	</head>
 	<body>
 		<a href="javascript:history.go(-1)">返回</a>
 		<h1 class="center">使用ss-rules透明翻墙的步骤</h1>
-			<?php
-				echo '<p class="center gray">更新时间：'.date("Y年m月d日 H:i:s",$updatetime_page).'</p>';
-			?>
+		<?php
+			echo '<p class="center gray">更新时间：'.date("Y年m月d日 H:i:s",$updatetime_page).'</p>';
+		?>
+		<div class="tabs">
+			<ul class="tab-links">
+				<li class="active gp-1"><a href="#tab1">事前准备</a></li>
+				<li class="gp-2"><a href="#tab2">配置ShadowSocks</a></li>
+				<li class="gp-3"><a href="#tab3">配置ChinaDNS</a></li>
+				<li class="sl"><a href="#tab4">常见问题</a></li>
+			</ul>
+			<div class="tab-content">
+				<div id="tab1" class="tab active">
+					<ul>
+						<li>准备好自己ShadowSocks的设置 <span class="hilight">服务器端口</span>、<span class="hilight">密码</span></li>
+						<li>记录下来你所在地的<span class="hilight">DNS</span> <a href="http://dns.ip.cn/" target="_blank">点击此处查找</a></li>
+						<li>对应机型的固件 <a href="firmware.php">下载页面</a></li>
+						<br />
+						准备好上述各项后，开始刷机/配置。
+					</ul>
+				</div>
+				<div id="tab2" class="tab">
+					<ul>
+						<li>在浏览器中登录路由器。</li>
+						<li>打开<span class="hilight">服务</span> <span class="octicon octicon-arrow-right"></span> <span class="hilight">ShadowSocks</span></li>
+						以下为各项配置说明：
+						<li><h3>全局设置</h3>
+							<ol>
+								<li>
+									<b>启用</b>：ShadowSocks的开关。
+								</li>
+								<li>
+									<b>使用配置文件</b>：可以指定路由器中的<tt>json</tt>格式的配置文件。
+									<br />
+									<span class="octicon octicon-info"></span> 设置比较麻烦，建议取消勾选此处，然后填写下面各项来配置。
+								</li>
+								<li>
+									<b>服务器地址</b>：已搭建好ShadowSocks服务端的VPS的 <tt>IP地址</tt>。
+								</li>
+								<li>
+									<b>服务器端口</b>：已搭建好ShadowSocks服务端的VPS的 <tt>ShadowSocks监听端口</tt>，已告知各位各自的端口。
+									<br />
+									<span class="octicon octicon-info"></span> <tt>8xxx</tt>端口用于连接<b>python版服务端</b>。
+									<br />
+									<span class="octicon octicon-info"></span> <tt>28xxx</tt>端口用于连接<b>go版服务端</b>。
+									<br />
+									<span class="octicon octicon-info"></span> 两种服务端本身没有明显区别，只是用于期待不同线路下取得更好的速度。
+								</li>
+								<li>
+									<b>本地端口</b>：任意的空闲端口，用于生成本地的代理端口，如无特殊需要，保持默认的<tt>1080</tt>即可。
+								</li>
+								<li>
+									<b>连接超时</b>：单位为秒，ShadowSocks的超时等待时间。
+									<br />
+									<span class="octicon octicon-info"></span> 建议设为较大（大于等于300）的值，有可能减少断流现象。
+								</li>
+								<li>
+									<b>密码</b>：已告知各位，格式为<tt>xxxx-xxxx-xxxx-xxxx</tt>。
+								</li>
+								<li>
+									<b>加密方式</b>：选择<tt>rc4-md5</tt>。
+								</li>
+								<li>
+									<b>代理方式</b>：选择<tt>忽略列表</tt>。
+									<br />
+									<span class="octicon octicon-info"></span> 忽略列表中为中国境内IP段，所有不在此IP段的TCP流量（所有境外流量）全部经由ShadowSocks代理。
+								</li>
+							</ol>
+						</li>
+						<li><h3>UDP转发</h3>
+							<span class="octicon octicon-info"></span> 通过UDP协议转发域名解析请求至VPS，并将解析结果返回至路由器，流量经由ShadowSocks所以不会<a href="http://zh.wikipedia.org/wiki/%E5%9F%9F%E5%90%8D%E6%9C%8D%E5%8A%A1%E5%99%A8%E7%BC%93%E5%AD%98%E6%B1%A1%E6%9F%93" target="_blank">被GFW污染域名</a>。
+							<br />
+							<span class="octicon octicon-info"></span> 启用后相当于多出来一个本地DNS域名解析服务器。
+							<br />
+							<span class="octicon octicon-info"></span> <b>只能</b>转发域名解析请求的UDP流量。
+							<ol>
+								<li>
+									<b>启用</b>：UDP转发的开关。
+								</li>
+								<li>
+									<b>UDP本地端口</b>：空闲的任意端口。
+									<br />
+									<span class="octicon octicon-info"></span> 建议值<tt>5353</tt>。
+								</li>
+								<li>
+									<b>UDP转发地址</b>：目标DNS，VPS接到来自路由器的域名解析请求后，会将请求转发到改地址解析域名。
+									<br />
+									<span class="octicon octicon-info"></span> 建议值<tt>8.8.8.8:53</tt>。
+								</li>
+							</ol>
+						</li>
+						<li><h3>访问控制</h3>
+							<span class="octicon octicon-info"></span> 通过IP地址指定允许/禁止使用ShadowSocks的设备，如无特殊需求无需设置。
+						</li>
+					</ul>
+					<br />
+					<p class="center"><span class="octicon octicon-info"></span> 所有设置点按<span class="btn">保存&应用</span>按钮后才会生效。</p>
+				</div>
+				<div id="tab3" class="tab">
+					<ul>
+						<li>在浏览器中登录路由器。</li>
+						<li>打开<span class="hilight">服务</span> <span class="octicon octicon-arrow-right"></span> <span class="hilight">ShadowSocks</span></li>
+						以下为各项配置说明：
+						<li><h3>基本设置</h3>
+							<ol>
+								<li>
+									<b>启用</b>：ChinaDNS的开关
+								</li>
+							</ol>
+						</li>
+					</ul>
+					<br />
+					<p class="center"><span class="octicon octicon-info"></span> 所有设置点按<span class="btn">保存&应用</span>按钮后才会生效。</p>
+				</div>
+			</div>
+		</div>
 			<p class="center">
 				<a href="#pre">事前准备</a><a href="#mods">需要修改的文件</a><a href="#steps">修改步骤、内容</a><a href="#faq">FAQ</a>
 			</p>
